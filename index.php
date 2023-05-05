602006b227365cf9a5005279c589593ff2668c0a
AnotheerDev <leger.julien94@gmail.com>
2023-05-05 09:48:42 +0200

    Glossaire SUPERGLOBALE

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!-- Glossaire SUPERGLOBALE :
+
+
+
+session_start() : est une fonction PHP qui démarre une session PHP en cours d'utilisation sur le serveur. Les sessions sont utilisées pour stocker des informations de manière persistante entre les différentes pages d'un site web pour un utilisateur spécifique. Une session est créée en générant un identifiant unique appelé session ID, qui est stocké sur le serveur et envoyé au navigateur de l'utilisateur sous forme de cookie.
+
+$_SESSION : est une variable PHP superglobale qui stocke les données de session. Cette variable peut être utilisée pour stocker et récupérer des informations utilisateur spécifiques telles que le nom d'utilisateur, l'adresse e-mail ou tout autre type d'informations que vous souhaitez stocker pour une session particulière.
+
+$_POST : est une autre variable PHP superglobale qui stocke les données qui ont été soumises à un script PHP via une méthode POST. Cette variable est souvent utilisée pour récupérer les données des formulaires HTML, où les utilisateurs saisissent des informations qui doivent être envoyées au serveur pour traitement.
+
+$_GET : est également une variable PHP superglobale, mais elle stocke les données qui ont été envoyées via la méthode GET. Les données sont récupérées à partir de l'URL de la page et sont généralement utilisées pour passer des paramètres entre différentes pages ou pour effectuer des requêtes de recherche.
+
+En résumé, session_start() est utilisé pour démarrer une session PHP et $_SESSION est utilisé pour stocker et récupérer des données de session. $_POST est utilisé pour récupérer les données envoyées via la méthode POST et $_GET est utilisé pour récupérer les données envoyées via la méthode GET.
+
+
+
+-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -33,4 +50,5 @@
             </p>
         </form>
     </body>
-</html>
+</html> 
+
