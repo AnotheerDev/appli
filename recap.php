869174b191c8dd2dae85411e399c4fee20a2f2a7
AnotheerDev <leger.julien94@gmail.com>
2023-05-09 08:54:13 +0200

    ajout table bootstrap

--- recap.php
+++ recap.php
@@ -6,6 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content ="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <title>Récapitulatif des produits</title>
     </head>
@@ -15,7 +16,7 @@
                 echo "<p>Aucun produit en session...</p>";
             }
             else{
-                echo "<table>",
+                echo "<table class='table table-striped'>",
                         "<thead>",
                             "<tr>",
                                 "<th>#</th>",
