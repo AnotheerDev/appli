721384e5e9a123d9e939565296a079a3630e3896
AnotheerDev <leger.julien94@gmail.com>
2023-05-09 10:41:58 +0200

    ajout commentaire index

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ En résumé, session_start() est utilisé pour démarrer une session PHP et $_SE
 
 
 -->
+<!-- -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,12 +24,15 @@ En résumé, session_start() est utilisé pour démarrer une session PHP et $_SE
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <!-- link à ajouter dans le head pour faire fonctionner bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title>Ajout produit</title>
 </head>
 
 <body>
+
+                <!-- nav bar réalisée par bootstrap -->
     <nav class="navbar" style="background-color: #e3f2fd;" fixed-top>
         <div class="container-fluid">
         <a class="navbar-brand" href="#">Faites vos courses : </a>
@@ -42,6 +47,11 @@ En résumé, session_start() est utilisé pour démarrer une session PHP et $_SE
         </div>
         </div>
     </nav>
+
+
+
+
+    
     <div class="container">
         <h1>Ajouter un produit</h1>
         <form action="traitement.php" method="post">
@@ -68,6 +78,8 @@ En résumé, session_start() est utilisé pour démarrer une session PHP et $_SE
             </p>
         </form>
     </div>
+
+                        <!-- script à ajouter en fin de body pour faire fonctionner bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
