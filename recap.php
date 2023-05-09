15e1525b456217c44bf364b40cb256575394b267
AnotheerDev <leger.julien94@gmail.com>
2023-05-09 09:33:43 +0200

    ajout des nav bar bootstrap dans index et recap

--- recap.php
+++ recap.php
@@ -6,17 +6,30 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content ="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <title>Récapitulatif des produits</title>
     </head>
     <body>
+        <nav class="navbar" style="background-color: #e3f2fd"; fixed-top>
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#">Récapitulatif des vos courses : </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" href="recap.php">Récapitulatif</a>
+                </div>
+            </div>
+            </div>
+        </nav>
         <?php
             if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
                 echo "<p>Aucun produit en session...</p>";
             }
             else{
-                echo "<table class='table table-striped'>",
+                echo "<table class='table table-striped mt-5'>",
                         "<thead>",
                             "<tr>",
                                 "<th>#</th>",
