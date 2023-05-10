7bb4c504bf16d35a96506e7540500f87e91e3966
AnotheerDev <leger.julien94@gmail.com>
2023-05-10 09:19:54 +0200

    modification index pour injecter template

--- index.php
+++ index.php
@@ -16,71 +16,44 @@ En résumé, session_start() est utilisé pour démarrer une session PHP et $_SE
 
 -->
 <!-- -->
+<?php
+session_start();
+ob_start();
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <!-- link à ajouter dans le head pour faire fonctionner bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-    <title>Ajout produit</title>
-</head>
-
-<body>
-
-                <!-- nav bar réalisée par bootstrap -->
-    <nav class="navbar" style="background-color: #e3f2fd;" fixed-top>
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#">Faites vos courses : </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                <a class="nav-link" href="recap.php">Récapitulatif</a>
-            </div>
-        </div>
-        </div>
-    </nav>
+?>
 
 
 
 
-    
-    <div class="container">
-        <h1>Ajouter un produit</h1>
-        <form action="traitement.php?action=add" method="post">
-            <p>
-                <label>
-                    Nom du produit :
-                    <input type="text" name="name">
-                </label>
-            </p>
-            <p>
-                <label>
-                    Prix du produit :
-                    <input type="number" step="any" name="price">
-                </label>
-            </p>
-            <p>
-                <label>
-                    Quantitée désirée :
-                    <input type="number" name="qtt" value="1">
-                </label>
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit">
-            </p>
-        </form>
-    </div>
+<div class="container">
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php?action=add" method="post">
+        <p>
+            <label>
+                Nom du produit :
+                <input type="text" name="name">
+            </label>
+        </p>
+        <p>
+            <label>
+                Prix du produit :
+                <input type="number" step="any" name="price">
+            </label>
+        </p>
+        <p>
+            <label>
+                Quantitée désirée :
+                <input type="number" name="qtt" value="1">
+            </label>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Ajouter le produit">
+        </p>
+    </form>
+</div>
 
-                        <!-- script à ajouter en fin de body pour faire fonctionner bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
+<?php
 
-</html>
+$content = ob_get_clean();
+$titre = "Ajout produit";
+require "template.php";
