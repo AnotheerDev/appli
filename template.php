999216b5082ad9b289a6fe4e23700083af7ac7f3
AnotheerDev <leger.julien94@gmail.com>
2023-05-10 09:19:11 +0200

    création du contenue template.php

---
+++ template.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- link à ajouter dans le head pour faire fonctionner bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+
+    <!-- nav bar réalisée par bootstrap -->
+    <nav class="navbar" style="background-color: #e3f2fd;" fixed-top>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Faites vos courses : </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link" href="recap.php">Récapitulatif</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    <?= $content ?>
+
+
+    <!-- script à ajouter en fin de body pour faire fonctionner bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
